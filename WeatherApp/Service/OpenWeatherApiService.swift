62436cc47ce550bf558043cfd7fbb8f37e23a284
Pierre Dias <cdias.pierre@gmail.com>
2023-09-12 13:18:45 -0300

    Removido Consumo service image - Open Weather Api

--- WeatherApp/Service/OpenWeatherApiService.swift
+++ WeatherApp/Service/OpenWeatherApiService.swift
@@ -33,24 +33,4 @@ public class OpenWeatherApiService {
                 }}            
         }.resume()
     }
-    
-    static func getImageFromUrl(urlString: String, completionHandler: @escaping (_ result: Result<UIImage, Error>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completionHandler(.failure(.urlError))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
-            DispatchQueue.main.async {
-                if error == nil,
-                   let data = dataResponse,
-                   let image = UIImage(data: data) {
-                    completionHandler(.success(image))
-                } else {
-                    completionHandler(.failure(.canNotParseData))
-                }
-            }
-        }.resume()
-                
-    }
 }
