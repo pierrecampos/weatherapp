1c5c598068483c70ffeef4fdfe87275ccdf277a0
Pierre Dias <cdias.pierre@gmail.com>
2023-09-11 21:52:15 -0300

    Consumo end-point para imagens - Weather Api

--- WeatherApp/Service/OpenWeatherApiService.swift
+++ WeatherApp/Service/OpenWeatherApiService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum Error: Swift.Error {
     case urlError
@@ -15,7 +16,7 @@ enum Error: Swift.Error {
 public class OpenWeatherApiService {
    
     static func getWeatherForescat(completionHandler: @escaping (_ result: Result<WeatherForecastModel, Error>) -> Void) {
-        let urlString = "https://run.mocky.io/v3/cf4a2eeb-e732-412e-9010-c55d70214f38"
+        let urlString = "https://run.mocky.io/v3/cf4a2eeb-e732-412e-9010-c55d70214f38"        
         guard let url = URL(string: urlString) else {
             completionHandler(.failure(.urlError))
             return
@@ -29,8 +30,27 @@ public class OpenWeatherApiService {
                     completionHandler(.success(resultData))
                 } else {
                     completionHandler(.failure(.canNotParseData))
-                }}
-            
+                }}            
         }.resume()
+    }
+    
+    static func getImageFromUrl(urlString: String, completionHandler: @escaping (_ result: Result<UIImage, Error>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completionHandler(.failure(.urlError))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { dataResponse, urlResponse, error in
+            DispatchQueue.main.async {
+                if error == nil,
+                   let data = dataResponse,
+                   let image = UIImage(data: data) {
+                    completionHandler(.success(image))
+                } else {
+                    completionHandler(.failure(.canNotParseData))
+                }
+            }
+        }.resume()
+                
     }
 }
