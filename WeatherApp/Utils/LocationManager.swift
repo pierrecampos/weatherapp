60515c50e60d1922d83ebca17bd78e11ff747109
Pierre Dias <cdias.pierre@gmail.com>
2023-10-04 19:20:17 -0300

    Implementado searchCoordinatesForAddress e searchAddressesForText para LocationManager

--- WeatherApp/Utils/LocationManager.swift
+++ WeatherApp/Utils/LocationManager.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 protocol LocationManagerDelegate: AnyObject {
     func updateUserLocation()
@@ -19,6 +20,9 @@ class LocationManager: NSObject {
     weak var delegate: LocationManagerDelegate?
     
     private lazy var cllocationManager = CLLocationManager()
+    private lazy var completer = MKLocalSearchCompleter()
+    private lazy var searchResults: [(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D?)] = []
+    private var searchCompletion: (([(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D?)]) -> Void)?
     var userLocationCoordinate: CLLocationCoordinate2D?
     
     
@@ -47,6 +51,23 @@ class LocationManager: NSObject {
             }
         }
     }
+    
+    public func searchAddressesForText(_ text: String,
+                                       completion: @escaping
+                                       ([(title: String?,
+                                          subtitle: String?,
+                                          coordinate: CLLocationCoordinate2D?)
+                                       ]) -> Void ) {
+        completer.delegate = self
+        completer.queryFragment = text
+        
+        if let userRegionCoordinate = self.userLocationCoordinate {
+            let region = MKCoordinateRegion(center: userRegionCoordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
+            completer.region = region
+        }
+        
+        searchCompletion = completion
+    }
 }
 
 extension LocationManager: CLLocationManagerDelegate {
@@ -67,5 +88,49 @@ extension LocationManager: CLLocationManagerDelegate {
             cllocationManager.stopUpdatingLocation()
             delegate?.updateUserLocation()
         }
+    }
+    
+    func searchCoordinatesForAddress(_ address: (title: String, subtitle: String),
+                                     completion: @escaping ((title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D?)?) -> Void) {
+        let searchRequest = MKLocalSearch.Request()
+        let entireAdddress = address.title + " " + address.subtitle
+        searchRequest.naturalLanguageQuery = entireAdddress
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response,
+               let firstPlacemark = response.mapItems.first?.placemark {
+                let coordinate = firstPlacemark.coordinate
+                completion((title: address.title, subtitle: address.subtitle, coordinate: coordinate))
+            } else {
+                completion(nil)
+            }
+        }
+    }
+    
+    func cancelSearchAddresses() {
+        if completer.isSearching {
+            completer.cancel()
+        }
+    }
+}
+
+extension LocationManager: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results.compactMap({ result in
+            let street = result.title
+            let subtitle = result.subtitle
+            let searchRequest = MKLocalSearch.Request(completion: result)
+            let coordinate = searchRequest.region.center
+            
+            return (title: street, subtitle: subtitle, coordinate: coordinate)
+        })
+        
+        searchCompletion?(searchResults)
+    }
+    
+    private func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        searchResults = []
+        searchCompletion?(searchResults)
     }
 }
