5120288bfa432db65657d15da153a0ad6259e5cd
Pierre Dias <cdias.pierre@gmail.com>
2023-10-10 21:16:56 -0300

    Criado arquivo Config para inserção da API KEY

---
+++ WeatherApp/Core/Config.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  WeatherApp
+//
+//  Created by Pierre Campos Dias on 10/10/23.
+//
+
+struct Config {
+    static let apiKey = "YOUR_API_KEY_HERE"
+}
--- WeatherApp/Services/OpenWeatherApiService.swift
+++ WeatherApp/Services/OpenWeatherApiService.swift
@@ -29,7 +29,7 @@ public class OpenWeatherApiService {
     }
    
     static func getWeatherForecast(_ latitude: Double, _ longitude: Double ,completionHandler: @escaping (_ result: Result<WeatherForecastModel, Error>) -> Void) {
-        let urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=d85f2a335b0845712dca0458c951343f&lang=pt_br&units=metric"        
+        let urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=\(Config.apiKey)&lang=pt_br&units=metric"        
         guard let url = URL(string: urlString) else {
             completionHandler(.failure(.urlError))
             return
